08f3ebbfc2049c87f5657d261034d2bff7dc222f
Myonghwa, Jung <kino811@gmail.com>
2016-09-08 16:18:56 +0900

    add Unity Project test lua in unity. use lua third-party-tool MoonSharp

---
+++ WowItemStringUnityProject/Assets/Scripts/TestLua.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using MoonSharp.Interpreter;
+
+public class TestLua : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    DoTest();
+	}
+	
+    void DoTest() {
+        string luaString = @"
+            -- defines a factorial function
+            function Fact(n)
+                if (n == 0) then
+                    return 1
+                else
+                    return n * Fact(n - 1)
+                end
+            end
+
+            return Fact(5)
+        ";
+
+        DynValue res = Script.RunString(luaString);
+
+        Debug.Log("lua-string evaluation result: " + res.Number);
+    }
+}
